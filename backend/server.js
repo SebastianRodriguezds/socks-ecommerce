e0107e973b07889b5c4f8885602d2d480f7893ee
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-08-14 12:16:20 -0300

    CRUD set up

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,51 @@ app.get('/products/:id', (req, res) => {
     }
 });
 
+// Upload product
+app.post('/products', (req, res)=> {
+    const imageName = req.body.image;
+    const imageUrl = imageName ? `http://localhost:5000/uploads/${imageName}`: null;
+
+    const newProduct = {
+        id: products.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.imageUrl,
+        category: req.body.category
+    };
+    products.push(newProduct);
+    fs.writeFileSync('./data/products.json', JSON.stringify(products, null, 2));
+    res.status(201).json(newProduct);
+});
+
+//Update product
+app.put('/products/:id', (req, res)=>{
+    const product = products.find(p => p.id === parseInt(req.params.id));
+
+    if (!product) return res.status(404).json({message: "Product not found"});
+
+    product.name = req.body.name ?? product.name;
+    product.description = req.body.description ?? product.description;
+    product.price = req.body.price ?? product.price;
+    product.image = req.body.image ?? product.image;
+
+    fs.writeFileSync('./data/products.json', JSON.stringify(products, null, 2));
+    res.json(product)
+});
+
+//delete product
+app.delete('/products/:id', (req, res)=> {
+    const index = products.findIndex(p => p.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).json({message: "Product not found"});
+
+    const deletedProduct  = products.splice(index, 1)[0];
+    fs.writeFileSync('./data/products.json', JSON.stringify(products, null, 2));
+    res.json(deletedProduct)
+});
+
+//socks images
+app.use('/uploads', express.static('uploads'));
+
+
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
