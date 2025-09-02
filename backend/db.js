cf992fdd13eaef8eb63198f9dec9177b35e79f45
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2025-09-02 13:38:04 -0300

    MONGO_URI updated and connection to MongoDB Atlas established

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (err) {
-        console.log("Failed to connect to MongoDB: ", err);
+        console.log("Failed to connect to MongoDB: ", err.message);
         process.exit(1);
     }
 };
